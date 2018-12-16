345c2e685c34d9a5008b3f7613fe326829612d95
j5a6i7n8i9s0h <21962504@student.uwa.edu.au>
2018-12-16 22:25:36 +0800

    basic frontend

---
+++ pixabay_image_finder/src/components/search/Search.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import TextField from "material-ui/TextField";
+import SelectField from "material-ui/SelectField";
+import MenuItem from "material-ui/MenuItem";
+
+class Search extends Component {
+  state = {
+    searchText: "",
+    amount: 15,
+    apiUrl: "https://pixabay.com/api",
+    apiKey: "11014569-e1c60c5b8e76514f9f4a16881",
+    images: []
+  };
+  onTextChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  render() {
+    return (
+      <div>
+        <TextField
+          name="searchText"
+          value={this.state.searchText}
+          onChange={this.onTextChange}
+          floatingLabelText="Search for images"
+          fullWidth={false}
+        />
+        <br />
+        <SelectField
+          floatingLabelText="Amount"
+          name="Amount"
+          value={this.state.amount}
+          onChange={this.onAmountChange}
+        >
+          <MenuItem value={5} primaryText="5" />
+          <MenuItem value={10} primaryText="10" />
+          <MenuItem value={15} primaryText="15" />
+          <MenuItem value={30} primaryText="30" />
+        </SelectField>
+      </div>
+    );
+  }
+}
+
+export default Search;
